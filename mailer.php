de7f285136234ee8afda4835d6526a8eab208ac0
Luan Vitola <tanukesensei@users.noreply.github.com>
2021-01-21 12:24:38 -0300

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -14,10 +14,10 @@ $mail = new PHPMailer(true);
 //Server settings
 //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
 $mail->isSMTP();                                            // Send using SMTP
-$mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
+$mail->Host       = host                                    // Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-$mail->Username   = '77d991b302be58';                     // SMTP username
-$mail->Password   = '98bb931744f0c6';                               // SMTP password
+$mail->Username   = username                                // SMTP username
+$mail->Password   = password                               // SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port       = 2525;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
